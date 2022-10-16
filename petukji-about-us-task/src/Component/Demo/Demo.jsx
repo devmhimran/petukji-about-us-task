881cf463d0c319ae09bcd2992cffef0cdb8a54d5
Mahmud Hasan Imran <mahmud.bubt.150@gmail.com>
2022-10-16 13:17:34 +0600

    Update Scroll Bar

--- petukji-about-us-task/src/Component/Demo/Demo.jsx
+++ petukji-about-us-task/src/Component/Demo/Demo.jsx
@@ -8,7 +8,7 @@ const Demo = () => {
                 <h1 className='text-center text-5xl font-bold'>Watch Our Demo</h1>
                 <p className='w-7/12 mx-auto my-6 text-[#5F6575] text-center'>We have 3 version of design on or app consumer view, restaurant view & pick up view. So, you don't need to hire any other designers to make your app.</p>
 
-                <div className="video__preview">
+                <div className="video__preview"  data-aos="fade-up" data-aos-anchor-placement="top-center">
                     <iframe className='rounded-[30px]' width="100%" height="650" src="https://www.youtube.com/embed/0HDqsGyz9d4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 </div>
             </div>
