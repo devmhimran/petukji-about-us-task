7d6787b1480ee468b738f947d752f8421e3c4fd9
Mahmud Hasan Imran <mahmud.bubt.150@gmail.com>
2022-10-18 15:16:42 +0600

    Update Api

--- petukji-about-us-task/src/Component/Contact/Contact.jsx
+++ petukji-about-us-task/src/Component/Contact/Contact.jsx
@@ -1,4 +1,4 @@
-import { Input, Textarea } from '@material-tailwind/react';
+import { Alert, Input, Textarea } from '@material-tailwind/react';
 import axios from 'axios';
 import React from 'react';
 import { toast, Toaster } from 'react-hot-toast';
@@ -18,7 +18,7 @@ const Contact = () => {
         }
 
         console.log(formAllData)
-        axios.post('http://localhost:5000/contact-form', formAllData)
+        axios.post('https://petuk-ji-server.vercel.app/contact-form', formAllData)
             .then(function (response) {
                 console.log(response);
                 toast.success('Successfully Send!')
@@ -28,6 +28,8 @@ const Contact = () => {
                 console.log(error);
                 toast.error("Something went wrong!")
             });
+
+        e.target.reset();
     }
 
 
@@ -39,12 +41,14 @@ const Contact = () => {
                 <div className="card border border-gray-200 rounded-[20px] mt-12 p-6" data-aos="fade-up"
                     data-aos-anchor-placement="center-bottom">
                     <div className="card-body">
+                        
                         <div className="grid grid-cols-2 gap-8">
                             <div className="google__map">
                                 <iframe className='w-full rounded-[15px]' src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13961.972396038982!2d77.6414242!3d28.9727558!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xa4ceeb13ddeb49d1!2sPetuk%20Ji%20Private%20Limited!5e0!3m2!1sen!2sbd!4v1666037794824!5m2!1sen!2sbd" height="450" allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
                             </div>
                             <div className="contact__form">
                                 <h2 className='text-2xl text-[#262524] font-semibold mb-6'>Write us a message</h2>
+                                <Alert className='my-4 text-xs' color="amber">You can only use <span className='bg-gray-300'>mahmud.bubt.150@gmail.com</span> to send contact info to get the bulk email. Because I used mailgun free subscription</Alert>
                                 <form onSubmit={handleContactForm}>
                                     <div className='form__input mb-6'>
                                         <Input color="teal" label="Enter Name" required name='name' />
